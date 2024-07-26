acf0239c72af937f4a90ba6fcdad02184eb3775a
Sinth <sinthmite@gmail.com>
2024-07-26 16:20:06 -0400

    functions added

--- src/Stats/stats.tsx
+++ src/Stats/stats.tsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
 import { useStateContext } from '../State/State';
+import { Fighter, Knight, Rogue, Berserker } from '../WarriorClass/WarriorClass';
+
 import './stats.css'
 export const Stats: React.FC = () =>{
     const { strength, setStrength, speed, setSpeed, agility, setAgility, health, setHealth, user, setUser } = useStateContext();
@@ -8,11 +10,11 @@ export const Stats: React.FC = () =>{
     
         <Fragment>
             <ul>
-                <li>💪 {strength}</li>
-                <li>🏃‍♂️ {speed}</li>
+                <li>💪 {user.getStrength()}</li>
+                <li>🏃‍♂️ {user.getSpeed()}</li>
                 <li>{user.name}</li>
-                <li>🧗‍♂️ {agility}</li>
-                <li>❤️ {health}</li>
+                <li>🧗‍♂️ {user.getAgility()}</li>
+                <li>❤️ {user.getHealth()}</li>
             </ul>
         </Fragment>
     )
